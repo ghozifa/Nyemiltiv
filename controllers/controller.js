ee59ec19a4d4c375d67f6f9c990c091aeba6f3f6
Ghozi Fattah Allauddin <ghozifattah@gmail.com>
2022-07-21 17:29:43 +0700

    test

--- controllers/controller.js
+++ controllers/controller.js
@@ -15,6 +15,72 @@ class Controller {
             })
     }
 
+    static login(req, res) {
+        let error = req.query.error;
+        res.render("login", { error });
+    }
+
+    static loginPost(req, res) {
+        const { email, password } = req.body;
+        let error = [];
+        if(!email) {
+            error.push ("Masukkan email terlebih dahulu");
+        }
+        if(!password) {
+            error.push ("Masukkan password terlebih dahulu");
+        }
+        if(error.length) {
+            return res.redirect(`/login?error=${error.join("; ")}`);
+        }
+        User.findOne({ 
+            where: { email: email }
+        })
+        .then((user) => { 
+            const error = "Email atau Password salah";  
+            if(user) {
+                // compare plain password sama hash password
+                const isPassValid = bcrypt.compareSync(password, user.password);
+                if(isPassValid) {
+                    return res.redirect("/");
+                }
+                return res.redirect(`/login?error=${error}`);
+                }
+            return res.redirect(`/login?error=${error}`);
+        })
+        .catch(err => {
+            res.send(err);
+        });        
+    }
+
+    static daftar(req, res) {
+        let error = req.query.error;
+        res.render("daftar", { error });
+    }
+
+    static daftarPost(req, res) {
+        const { email, password, role } = req.body;    
+        User.create({ email, password, role })
+        .then(() => {
+            res.redirect("/login")
+        })
+        .catch(err => {
+            let error;
+            if(err.name == "SequelizeValidationError") {
+                error = err.errors.map(el => {
+                    return el.message
+                }).join("; ");
+                res.redirect(`/daftar?error=${error}`);
+            } else if (err.name == "SequelizeUniqueConstraintError") {
+                error = err.errors.map(el => {
+                    return el.message
+                }).join("; ");
+                res.redirect(`/daftar?error=${error}`);
+            } else {
+                res.send(err);
+            }
+        });
+    }
+
     static food(req, res) {
         Category.findByPk(1, {
             include: Product
@@ -108,75 +174,7 @@ class Controller {
 
     static buyProducts(req, res) {
 
-    }
-
-
-    static login(req, res) {
-        let error = req.query.error;
-        res.render("login", { error });
-    }
-
-    static loginPost(req, res) {
-        const { email, password } = req.body;
-        let error = [];
-        if(!email) {
-            error.push ("Masukkan email terlebih dahulu");
-        }
-        if(!password) {
-            error.push ("Masukkan password terlebih dahulu");
-        }
-        if(error.length) {
-            return res.redirect(`/login?error=${error.join("; ")}`);
-        }
-        User.findOne({ 
-            where: { email: email }
-        })
-        .then((user) => { 
-            const error = "Email atau Password salah";  
-            if(user) {
-                // compare plain password sama hash password
-                const isPassValid = bcrypt.compareSync(password, user.password);
-                if(isPassValid) {
-                    return res.redirect("/");
-                }
-                return res.redirect(`/login?error=${error}`);
-                }
-            return res.redirect(`/login?error=${error}`);
-        })
-        .catch(err => {
-            res.send(err);
-        });        
-    }
-
-    static daftar(req, res) {
-        let error = req.query.error;
-        res.render("daftar", { error });
-    }
-
-    static daftarPost(req, res) {
-        const { email, password, role } = req.body;    
-        User.create({ email, password, role })
-        .then(() => {
-            res.redirect("/login")
-        })
-        .catch(err => {
-            let error;
-            if(err.name == "SequelizeValidationError") {
-                error = err.errors.map(el => {
-                    return el.message
-                }).join("; ");
-                res.redirect(`/daftar?error=${error}`);
-            } else if (err.name == "SequelizeUniqueConstraintError") {
-                error = err.errors.map(el => {
-                    return el.message
-                }).join("; ");
-                res.redirect(`/daftar?error=${error}`);
-            } else {
-                res.send(err);
-            }
-        });
-    }
-
+    }    
 }
 
 module.exports = Controller;
--- routes/routes.js
+++ routes/routes.js
@@ -2,11 +2,10 @@ const Controller = require("../controllers/controller");
 const router = require("express").Router();
 
 router.get("/", Controller.home); // Home index toko
+router.get("/login", Controller.login); // page form login
 router.post("/login", Controller.loginPost); // login
 router.get("/daftar", Controller.daftar); // daftar User
 router.post("/daftar", Controller.daftarPost); // Input data Daftar User
-router.get("/daftar", Controller.daftar);
-router.post("/daftar", Controller.daftarPost);
 router.get("/food", Controller.food); // Product with categories food
 router.get("/beverage", Controller.beverage); // Product with categories beverage
 router.get("/products/:id", Controller.productsDetailById); // Product with detail include category
