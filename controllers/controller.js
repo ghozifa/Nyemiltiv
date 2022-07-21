05c5b2af252cc9be8d34d6398ae01a4b08c6cf5f
Ghozi Fattah Allauddin <ghozifattah@gmail.com>
2022-07-21 14:00:40 +0700

    nambah login dan daftar

--- controllers/controller.js
+++ controllers/controller.js
@@ -6,6 +6,25 @@ class Controller {
         res.render("home");
     }
 
+    static login(req, res) {
+        res.render("login");
+    }
+
+    static daftar(req, res) {
+        res.render("daftar");
+    }
+
+    static daftarPost(req, res) {
+        const { email, password } = req.body;
+        UserProduct.create({ email, password })
+        .then(() => {
+            res.redirect("/login")
+        })
+        .catch(err => {
+            res.send(err);
+        })
+    }
+
 }
 
 module.exports = Controller;
--- routes/routes.js
+++ routes/routes.js
@@ -2,5 +2,8 @@ const Controller = require("../controllers/controller");
 const router = require("express").Router();
 
 router.get("/", Controller.home); // Home index toko
+router.get("/login", Controller.login); // Login User
+router.get("/daftar", Controller.daftar);
+router.post("/daftar", Controller.daftarPost);
 
 module.exports = router;
