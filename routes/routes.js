afe75d270b03b4baf555a5eda1f7f51782967dc0
Ghozi Fattah Allauddin <ghozifattah@gmail.com>
2022-07-21 16:55:51 +0700

    nambah fitur daftar dan login

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,8 @@ const router = require("express").Router();
 
 router.get("/", Controller.home); // Home index toko
 router.get("/login", Controller.login); // Login User
-router.get("/daftar", Controller.daftar);
-router.post("/daftar", Controller.daftarPost);
+router.post("/login", Controller.loginPost); // login
+router.get("/daftar", Controller.daftar); // daftar User
+router.post("/daftar", Controller.daftarPost); // Input data Daftar User
 
 module.exports = router;
